8f29bf87df776a557cc267132fc45e6cdc150257
thomasbuo <thomasbuo@gmail.com>
2018-03-12 12:17:40 +0100

    Initial commit

---
+++ src/main/java/core/Core.java
@@ -0,0 +1,20 @@
+package core;
+
+import graphical_interface.GraphicalInterface;
+import datastructures.*;
+
+/**
+ * 
+ * @author thomas 
+ * main class
+ * it calls the interface
+ *
+ */
+
+public class Core {
+	
+	public static void main(String[] args) {
+		GraphicalInterface gui = new GraphicalInterface();
+		gui.setVisible(true);
+	}
+}
